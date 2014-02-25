73b719b72de9f71bc66269e63291c202ab251ecf
aisassigit <aisassi@aisassiMac.local>
2014-02-25 13:52:38 -0700

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('codeCourseProtoApp',['ui.ace','ui.bootstrap'])
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  })
+
+
