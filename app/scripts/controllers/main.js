a9909309bf866612c55cf02e456dd41ac0533e98
aisassi <adolfo@rocketmade.com>
2014-02-26 12:13:58 -0700

    Added code to feedback panel

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,15 +9,23 @@ angular.module('codeCourseProtoApp',['ui.ace','ui.bootstrap'])
     ];
     $scope.aceLoaded = function(_editor) {
         console.log('editor loaded...');
+        //any programmatic configuration?
         $scope.codeEdit = _editor;
     };
 
     $scope.aceChanged = function(e) {
-        console.log('submit code here...')
+        console.log('submit code here...');
+        //any actions that need to take place on console change?
     };
 
     $scope.saveCode = function(){
-        console.log($scope.codeEdit.getValue());
+        var code = $scope.codeEdit.getValue();
+        var ln = $scope.codeEdit.selection.getCursor().row + 1;
+        var col = $scope.codeEdit.selection.getCursor().column;
+        console.log(code);
+        //fake http rest call
+        $scope.feedback = 'Error at line ' +ln+' : '+col;
+        //setTimeout(function(){return },30000)
     };
 
   })
