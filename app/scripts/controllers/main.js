3e3afb9c6fd3aa470a249a2a42a1e945a962a498
aisassi <adolfo@rocketmade.com>
2014-03-06 17:23:31 -0700

    Updated prototype to get closer to wireframes before using code as a starting point for new code repo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -27,66 +27,71 @@ var appControllers = angular.module('appControllers', []);
 
     appControllers.controller('TabsCtrl',['$scope',function($scope){
         $scope.tabs = [
-            { title:'Dynamic Title 1', content:'Dynamic content 1' },
-            { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+            {title:'Editor',content:'', id:0, active:true}
         ];
-
-        $scope.showCodeMirror = function(n) {
-            if($scope['codeMirror'+1] === undefined){
-                var cc = document.getElementById('cm'+n); //this is ugly as heck, but is how cm documentation shows init
-                var cm = CodeMirror.fromTextArea(cc);
-                // Options
-                cm.setOption('firstLineNumber',0);
-                cm.setOption('theme','night');
-                cm.setOption('lineNumbers',true);
-                cm.setSize(500, 100);
-                cm.setOption('extraKeys',{"Enter": function(){console.log('special Enter handling here...')}});
-                $scope['codeMirror'+n] = cm;
+        $scope.editors = [];
+        $scope.terminals = [];
+        $scope.addTab = function(){
+            var tabId = $scope.tabs.length;
+            var tabLabel = tabId+1;
+            //console.log('next tab id: '+tabId)
+            $scope.tabs.splice(tabId,0,{title:'Editor '+tabLabel, content:'', id:tabId, active:true});
+        },
+        $scope.closeTab = function(id){
+            //console.log('remove id: '+id);
+            if($scope.tabs.length !== 1){
+                $scope.tabs.splice(id,1);
             }
-        };
+        },
 
         $scope.aceLoaded = function(_editor) {
-            console.log('editor loaded...');
+            console.log('instance of editor loaded...');
+            $scope.editors.push(_editor);
+        };
 
+        $scope.terminalLoaded = function(_editor){
             _editor.commands.addCommand({
                 name: 'enterKey',
                 bindKey: {win: 'Enter',  mac: 'Enter'},
-                exec: function(editor) {
-
-                    var code = $scope.codeEdit.getValue();
+                exec: function() {
+                    var terminalInst, code, ln, col;
+                    //terminalInst = $scope.terminals[id];
+                    code = _editor.getValue();
                     if(code !== ''){
-                        var ln = $scope.codeEdit.selection.getCursor().row + 1;
-                        var col = $scope.codeEdit.selection.getCursor().column;
-                        $scope.codeEdit.setValue('Error at line: ' +ln+' : column: '+col);
+                        ln = _editor.selection.getCursor().row + 1;
+                        col = _editor.selection.getCursor().column;
+                        _editor.setValue('Error at line: ' +ln+' : column: '+col);
                     }else{
-                        $scope.codeEdit.setValue('');
+                        _editor.setValue('');
                     }
 
                 },
                 readOnly: true // false if this command should not apply in readOnly mode
             });
-
-            $scope.codeEdit = _editor;
-        };
+            $scope.terminals.push(_editor)
+        },
 
         $scope.aceChanged = function(e) {
             console.log('submit code here...');
             //any actions that need to take place on console change?
         };
 
-        $scope.saveCode = function(){
-            var code = $scope.codeEdit.getValue();
+        $scope.terminalChanged = function(e) {
+            console.log('terminal update here...');
+            //any actions that need to take place on console change?
+        };
+
+        $scope.saveCode = function(id){
+            var editorInst,code,ln,col;
+            editorInst = $scope.editors[id];
+            code = editorInst.getValue();
             if(code !== ''){
-                var ln = $scope.codeEdit.selection.getCursor().row + 1;
-                var col = $scope.codeEdit.selection.getCursor().column;
+                ln = editorInst.selection.getCursor().row + 1;
+                col = editorInst.selection.getCursor().column;
                 $scope.feedback = 'Error at line: ' +ln+' : column: '+col;
             }else{
                 $scope.feedback = '';
             }
-        };
-
-        $scope.codemirrorLoaded = function(_editor){
-            //any on load code here...
         };
 
         $scope.sendTerminal = function(){
