f95b120622dcecdbb1812d4b6bab2c78c97f1639
aisassi <adolfo@rocketmade.com>
2014-03-14 13:34:03 -0600

    Added jQuery terminal

--- app/scripts/directives/appDirectives.js
+++ app/scripts/directives/appDirectives.js
@@ -21,3 +21,34 @@ appDirectives.directive('onEnter',[function(){
         link:linkFn
     };
 }]);
+
+
+appDirectives.directive('terminal',[function(){
+
+    var linkFn = function(scope,element,attrs) {
+        element.terminal(function(command, term) {
+            if (command !== '') {
+                try {
+                    var result = window.eval(command);
+                    if (result !== undefined) {
+                        term.echo(new String(result));
+                    }
+                } catch(e) {
+                    term.error(new String(e));
+                }
+            } else {
+                term.echo('');
+            }
+        }, {
+            greetings: 'Terminal emulator',
+            height: 200,
+            width: 590,
+            prompt: 'student44$> '});
+    };
+    return {
+        scope:{
+            onenter:'&onEnter'
+        },
+        link:linkFn
+    };
+}]);
